fd35a6c994cfc3e5f24ffe8886b7a3c9e60ae59d
EC2 Default User <ec2-user@ip-172-31-28-142.ec2.internal>
2023-02-02 10:07:35 +0000

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,31 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    if current_user
+      flash[:notice] = "Sign in successfully."
+      user_path(resource)  #指定したいパスに変更
+    else
+      flash[:notice] = "Sign up successfully."
+      user_path(resource) #指定したいパスに変更
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    flash[:notice] = "Signed out successfully."
+    root_path
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [ :email, :name, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
+end
+
+
